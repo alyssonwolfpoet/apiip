8888396759eaba3b631788c4f41f42d729d7c33d
Alysson <alyssos@hotmail.com>
2026-07-13 16:55:07 -0300

    test ip

--- ip.py
+++ ip.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 @app.get("/")
-async def inicio(request: Request):
-    ip = request.client.host
-    return {"ip": ip}
+async def info(request: Request):
+    return {
+        "ip": request.client.host,
+        "porta": request.client.port,
+        "metodo": request.method,
+        "url": str(request.url),
+        "caminho": request.url.path,
+        "headers": dict(request.headers),
+        "cookies": request.cookies,
+        "query": dict(request.query_params),
+    }
